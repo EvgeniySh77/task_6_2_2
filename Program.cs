f002cafa95aca655bb1d29deafe147e3e9f2d8e5
EvgeniySh77 <evgen.sh77@mail.ru>
2022-02-05 15:45:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_6_2_2
+{
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Pen pen = new Pen();
+            Console.WriteLine($"{pen.color} {pen.cost}");
+
+            pen = new Pen("Зеленый", 80);
+            Console.WriteLine($"{pen.color} {pen.cost}");
+
+            Console.ReadKey();
+        }
+    }
+}
